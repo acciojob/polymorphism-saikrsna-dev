eb80016c13816c02139d05968218674840bba3cf
Saikrsna <109585296+saikrsna-dev@users.noreply.github.com>
2023-04-15 00:04:13 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-
+  public static class Product{
+    public int product(int x, int y) {}
+    public int product(int x, int y, int z) {}
+    public double product(double x, double y) {}
+  }
+  public static void main(String args[]){
+    Product p = new Product();
+    p.product(2, 3);
+    p.product(2, 3, 4);
+    p.product(2.3, 5..5);
+  }
 }
